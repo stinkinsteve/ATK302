c8eac8e38ae228c057a6bf3fb6cef73ab2fe3df7
stinkinsteve <54410403+stinkinsteve@users.noreply.github.com>
2019-10-03 04:55:39 -0500

    q
    asdf

--- p5/mp2/sketch.js
+++ p5/mp2/sketch.js
@@ -1,36 +1,69 @@
+var img1, img2, img3, img4, img5 ;
 var myState = 0 ;
 var timer = 0 ;
+
 function setup() {
   // put setup code here
+  img1 = loadImage("assets/bluebk.jpg");
+  img2 = loadImage("assets/darkwaves.jpg");
+  img3 = loadImage("assets/goldwaves.jpg");
+  img4 = loadImage("assets/mainbk.jpg");
+  img5 = loadImage("assets/oceanwaves.jpg");
+  createCanvas(800,800);
+  imageMode(CENTER);
 }
 
 function draw() {
   // put drawing code here
-switch(myState) (
+  image(img1, width, height, 200, 200);
+switch(myState) {
   case 0:
-  timer++ ;
-  if (timer > 200) {
+  image(img1, width, height, 200, 200);
+    timer++ ;
+    if (timer > 200) {
     myState = myState + 1 ;
   }
   break;
 
   case1:
+  image(img2, width, height, 200, 200);
+    timer++ ;
+    if (timer > 200) {
+    myState = myState + 1 ;
+  }
   break;
 
   case2:
+  image(img3, width, height, 200, 200);
+    timer++ ;
+    if (timer > 200) {
+    myState = myState + 1 ;
+  }
   break
 
   case3:
+  image(img4, width, height, 200, 200);
+    timer++ ;
+    if (timer > 200) {
+    myState = myState + 1 ;
+  }
   break;
 
   case4:
+  image(img5, width, height, 200, 200);
+    timer++ ;
+    if (timer > 200) {
+    myState = myState + 1 ;
+  }
   break;
+
 }
 
 }
 
 function mouseReleases() {
-  if (myState ==0) {
+  myState++ ;
+  if (myState > 4) {
     myState = 1 ;
   }
 }
